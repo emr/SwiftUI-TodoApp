eb12a0e19a701ba7779956a5bd07a58235b36e57
Emre Akinci <eemreakinci@gmail.com>
2020-06-25 19:12:06 +0300

    Move TodoStatus enum into Todo struct

--- TodoApp/Models/Todo.swift
+++ TodoApp/Models/Todo.swift
@@ -1,15 +1,15 @@
 import SwiftUI
 
-enum TodoStatus {
-    case todo
-    case done
-}
-
 struct Todo: Identifiable {
     var id: Int
-    var status: TodoStatus
+    var status: Status
     var title: String
     var description: String
     var createdAt: Date
     var dueDate: Date
+    
+    enum Status {
+        case todo
+        case done
+    }
 }
