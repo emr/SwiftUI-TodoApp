26581c985639e6d1bfaee50e27ee7413ff61240c
Emre Akinci <eemreakinci@gmail.com>
2020-06-27 02:37:11 +0300

    [api] Add web profiler

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
