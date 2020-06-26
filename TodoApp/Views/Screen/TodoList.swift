e88fdee38ab82fcab33ee4b709777d23426d595d
Emre Akinci <eemreakinci@gmail.com>
2020-06-26 11:32:36 +0300

    Show create sheet in list screen

--- TodoApp/Views/Screen/TodoList.swift
+++ TodoApp/Views/Screen/TodoList.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct TodoList: View {
     @State var todos: [Todo]
     @State var showCreateScreen = false
+    @State private var newTodo: Todo = Todo(id: 0, status: .done, title: "", description: "", createdAt: Date(), dueDate: Date())
     
     var body: some View {
         NavigationView {
@@ -15,16 +16,28 @@ struct TodoList: View {
             }
                 .navigationBarTitle(Text("Todos"))
                 .navigationBarItems(
-                    trailing: Button(action: {
-                        self.showCreateScreen.toggle()
-                    }) {
+                    trailing: Button(action: { self.showCreateScreen = true }) {
                         Image(systemName: showCreateScreen ? "plus.circle.fill" : "plus.circle")
                     }
                 )
                 .sheet(isPresented: $showCreateScreen, content: {
-                    CreateTodo()
+                    EditTodo(
+                        intent: .create,
+                        todo: self.$newTodo,
+                        onSave: self.handleSave,
+                        validate: self.validateNewTodo
+                    )
                 })
         }
+    }
+    
+    private func handleSave() {
+        self.todos.append(self.newTodo)
+        showCreateScreen = false
+    }
+    
+    private func validateNewTodo() -> Bool {
+        !newTodo.title.isEmpty && !newTodo.description.isEmpty
     }
 }
 
