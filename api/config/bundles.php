c29f6f5f6a0a6d7771d6acb32e037906d37ced15
Emre Akinci <eemreakinci@gmail.com>
2020-06-26 16:13:37 +0300

    [api] Add annotations and doctrine odm

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
