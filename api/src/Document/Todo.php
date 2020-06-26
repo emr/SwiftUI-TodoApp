bdb96e72599cd56a74f4f955a0d9e6d458f876dd
Emre Akinci <eemreakinci@gmail.com>
2020-06-26 17:26:26 +0300

    [api] Create restful api for Todo resource

---
+++ api/src/Document/Todo.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Document;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Document;
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Field;
+use Doctrine\ODM\MongoDB\Mapping\Annotations\Id;
+
+/**
+ * @ApiResource()
+ * @Document(collection="todos")
+ */
+class Todo
+{
+    /**
+     * @Field(type="string")
+     */
+    public string $title;
+
+    /**
+     * @Field(type="string")
+     */
+    public string $description;
+
+    /**
+     * @Field(type="string")
+     */
+    public string $status;
+
+    /**
+     * @Field(type="date")
+     */
+    public \DateTimeInterface $dueDate;
+
+    /**
+     * @Id(type="string")
+     */
+    private string $id;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+}
