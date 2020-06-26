d223616acd896bd282cc865cf955d082f9f62ab2
Emre Akinci <eemreakinci@gmail.com>
2020-06-26 16:17:11 +0300

    [api] Add maker bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -4,4 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
